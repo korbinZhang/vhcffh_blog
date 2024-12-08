1ad06d7aa83f70ec117558e151d8b838b5c26b1e
korbin <korbin.zhang@outlook.com>
2024-12-08 18:53:59 +0800

    主页

---
+++ theme/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { usePageData } from 'rspress/runtime'
+import Theme, { Link } from 'rspress/theme'
+
+const PostList = () => {
+  const { siteData } = usePageData()
+  let pageList: {
+    date: string
+    title: string
+    route: string
+  }[] = []
+
+  for (const page of siteData.pages) {
+    if (page.frontmatter.date) {
+      pageList.push({
+        date: page.frontmatter.date as string,
+        title: page.title,
+        route: page.routePath,
+      })
+    }
+  }
+  pageList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+  pageList = pageList.slice(0, 5)
+  return (
+    <div>
+      <ul>
+        {pageList.map((page) => (
+          <li key={page.route}>
+            <span> {page.date.slice(0, 10)} </span>
+            &nbsp;
+            <Link href={page.route}>{page.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const Layout = () => {
+  const { page } = usePageData()
+  const { frontmatter } = page
+  if (frontmatter.layout === 'home') {
+    return <Theme.Layout afterDocContent={PostList()} />
+  }
+  return <Theme.Layout />
+}
+
+export default {
+  ...Theme,
+  Layout,
+}
+
+export * from 'rspress/theme'
