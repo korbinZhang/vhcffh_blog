57ce4aea4201b04b7ccd3f93f19402ff7d556d11
korbin <korbin.zhang@outlook.com>
2024-12-14 22:32:24 +0800

    favicon

--- rspress.config.ts
+++ rspress.config.ts
@@ -5,9 +5,15 @@ export default defineConfig({
   // 文档根目录
   root: 'content',
   plugins: [katex()],
+  head: [
+    '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">',
+    '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">',
+    '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">',
+    '<link rel="manifest" href="/site.webmanifest">',
+  ],
   themeConfig: {
     nav: [],
     outline: false,
     hideNavbar: 'always',
-  }
+  },
 })
