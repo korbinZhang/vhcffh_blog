aca6d0da2e8eb48050adf42bc63ca89e7bd8af6d
korbin <korbin.zhang@outlook.com>
2025-04-30 20:35:01 +0800

    theme: google ads

--- rspress.config.ts
+++ rspress.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">',
     '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">',
     '<link rel="manifest" href="/site.webmanifest">',
+    '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3597458182538053" crossorigin="anonymous"></script>',
   ],
   themeConfig: {
     outlineTitle: '目录',
