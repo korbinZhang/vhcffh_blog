7464df262e1529a54ad32f1283ac61a50377c4d4
korbin <korbin.zhang@outlook.com>
2025-06-15 18:37:58 +0800

    blog: dify chat

---
+++ content/blog/2025/components/dify-chat.tsx
@@ -0,0 +1,17 @@
+const DifyChat = () => {
+  return (
+    <iframe
+      src="http://dify.vhcffh.com/chatbot/6zTQWyhhe0oxJNpg"
+      style={{
+        width: '100%',
+        height: '100%',
+        minHeight: '700px',
+        overflow: 'hidden',
+      }}
+      allow="microphone"
+    />
+  )
+}
+
+export default DifyChat
+
