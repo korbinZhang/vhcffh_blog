e66167dafae16b86aa8b11f24591bd6740371bd1
korbin <korbin.zhang@outlook.com>
2026-04-28 23:46:18 +0800

    books page

--- content/book/index.tsx
+++ content/book/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@theme";
+
 interface Book {
   title: string;
   url: string;
@@ -7,35 +9,28 @@ interface Book {
 
 const books: Book[] = [
   {
-    title: '100-gcc-tips',
-    url: '100-gcc-tips',
+    title: "100-gcc-tips",
+    url: "100-gcc-tips",
   },
 ];
 
 const BookList: React.FC = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+    <ol>
       {books.map((book) => {
         return (
-          <a
-            key={book.title}
-            className="relative bg-blue-500 m-2 pt-15 pb-35 rounded-lg hover:shadow-lg hover:-translate-y-1"
-            href={`book/${book.url}`}
-          >
-            <span className="absolute left-0 top-0 w-2 h-full bg-black/30 rounded-l-lg" />
-            <div className="ml-2 pl-2 bg-blue-300 h-20 line-clamp-5">
-              {book.title}
-            </div>
-          </a>
+          <li key={book.url}>
+            <Link href={`book/${book.url}`}>{book.url}</Link>
+          </li>
         );
       })}
-    </div>
+    </ol>
   );
 };
 
 export const frontmatter = {
-  date: '2025-08-17',
-  description: '值得阅读的一些笔记',
+  date: "2025-08-17",
+  description: "值得阅读的一些笔记",
   title: "Korbin's book",
   sidebar: false,
   footer: false,
